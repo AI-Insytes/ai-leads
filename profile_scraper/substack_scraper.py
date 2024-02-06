955a4c9898e21c0dc58a94e2aa96a92e61627dd3
Caleb Hemphill <calebhemp@gmail.com>
2024-02-06 03:00:25 -0500

    Update scraper

--- profile_scraper/substack_scraper.py
+++ profile_scraper/substack_scraper.py
@@ -1,28 +1,47 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+
 
 def search_profiles(search_query):
     """
     
     """
 
+    profiles_markup = []
+
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
-
         page.goto("https://substack.com/home")
 
         page.get_by_placeholder("Search...").fill(search_query)
         page.get_by_placeholder("Search...").press("Enter")
         page.get_by_role("button", name="People").click()
 
-        profiles = page.query_selector_all("div.pencraft.pc-display-flex.pc-flexDirection-column.pc-paddingBottom-16.pc-reset")
+        profiles_count = len(page.query_selector_all("div.pencraft.pc-display-flex.pc-flexDirection-column.pc-paddingBottom-16.pc-reset"))
+        
+        for index in range(0, 6):
 
-        profiles[0].click()
-        page.go_back()
-        profiles[1].click()
+            profiles_links = page.query_selector_all("div.pencraft.pc-display-flex.pc-flexDirection-column.pc-paddingBottom-16.pc-reset")
 
+            profiles_links[index].click()
 
+            if page.query_selector("#trigger5"):
+                page.click("//*[@id='trigger5']")
+                markup = page.content()
+                profiles_markup.append(markup)
+
+            page.go_back()
+
+        browser.close()
     
+    return profiles_markup
+
+def extract_profiles(profiles_markup):
+    """
+    
+    """
+
 
 test_query = "blockchain"
 search_profiles(test_query)
