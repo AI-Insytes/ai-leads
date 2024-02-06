ceffd0c6a0a17c67d884f7537d3afa5c9a1de5d9
Caleb Hemphill <calebhemp@gmail.com>
2024-02-06 03:59:33 -0500

    Add function to extract profile data

--- profile_scraper/substack_scraper.py
+++ profile_scraper/substack_scraper.py
@@ -42,6 +42,41 @@ def extract_profiles(profiles_markup):
     
     """
 
+    profile_objects = []
+
+    for markup in profiles_markup:
+
+        profile = {}
+
+        soup = BeautifulSoup(markup, "html.parser")
+
+        # extract name
+        name_markup = soup.select("h1")
+        profile["name"] = name_markup.get_text()
+
+        # extract blog link
+        if soup.select("div.pencraft.pc-display-flex.pc-flexDirection-column.pc-gap-16.pc-reset"):
+            blog_link_markup = soup.select("div.pencraft.pc-display-flex.pc-flexDirection-column.pc-gap-16.pc-reset a")
+            profile["blog"] = blog_link_markup.get("href")
+
+        # extract profile links
+        profile_links_markup = soup.select("#dialog6 div div")
+        profile_links = {}
+        for link in profile_links_markup:
+            link_name_markup = link.select("div")
+            link_name = link_name_markup.get_text()
+            profile_links[link_name] = link.get("href")
+        profile["profiles"] = profile_links
+
+        profile_objects.append(profile)
+
+    return profile_objects
+
+def main(search_query):
+    """
+    
+    """
+
 
 test_query = "blockchain"
 search_profiles(test_query)
