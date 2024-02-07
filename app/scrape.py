4ce8052a9ff52ce69cccc033c2b4cb474a624dc1
Caleb Hemphill <calebhemp@gmail.com>
2024-02-06 20:36:26 -0500

    Uncomment code

--- app/scrape.py
+++ app/scrape.py
@@ -1,6 +1,6 @@
 from profile_scraper.wordpress_scraper import main as wordpress_scraper
 from profile_scraper.substack_scraper import main as substack_scraper
-# from pseudobase.pseudobase import add_to_leads
+from pseudobase.pseudobase import add_to_leads
 
 
 def scrape_leads(keyword, wordpress=True, substack=True):
@@ -11,11 +11,11 @@ def scrape_leads(keyword, wordpress=True, substack=True):
     if wordpress:
         wordpress_results = wordpress_scraper(keyword)
         print(wordpress_results)
-        # add_to_leads(wordpress_results, "wordpress")
+        add_to_leads(wordpress_results, "wordpress")
     if substack:
         substack_results = substack_scraper(keyword)
         print(substack_results)
-        # add_to_leads(substack_results, "substack")
+        add_to_leads(substack_results, "substack")
 
 
 test_keyword = "blockchain"
